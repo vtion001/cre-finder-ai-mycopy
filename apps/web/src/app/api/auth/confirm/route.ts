7018415685556bcf58b48d6b6e299e40bb8f39b4
nilopro <danilocoding@pm.me>
2025-05-24 11:59:55 -0300

    fix auth redirect

--- apps/web/src/app/api/auth/confirm/route.ts
+++ apps/web/src/app/api/auth/confirm/route.ts
@@ -1,37 +1,52 @@
 import type { EmailOtpType } from "@supabase/supabase-js";
-import type { NextRequest } from "next/server";
-
 import { updateUser } from "@v1/supabase/mutations";
 import { createClient } from "@v1/supabase/server";
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+import { NextResponse } from "next/server";
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/";
+  const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? "/dashboard";
 
-  if (token_hash && type) {
+  if (code || token_hash) {
     const supabase = createClient();
 
-    const { error, data } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-    if (!error) {
-      console.log("verified", type, data);
+    let error = null;
+    let data = null;
 
-      if (type === "email_change" && data.user) {
-        console.log("updating user email", data.user.email, data.user.id);
+    if (code) {
+      const codeResult = await supabase.auth.exchangeCodeForSession(code);
+      error = codeResult.error;
+      data = codeResult.data;
+    } else if (token_hash && type) {
+      const tokenResult = await supabase.auth.verifyOtp({
+        token_hash,
+        type,
+      });
+      error = tokenResult.error;
+      data = tokenResult.data;
+    }
+
+    if (!error) {
+      if (type === "email_change" && data?.user) {
         await updateUser(supabase, { email: data.user.email });
         revalidatePath(`user_${data.user.id}`);
       }
 
-      redirect(next);
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+      if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      }
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
-  // redirect the user to an error page with some instructions
-  redirect("/auth/auth-code-error");
+  return NextResponse.redirect(`${origin}?error=auth-code-error`);
 }
