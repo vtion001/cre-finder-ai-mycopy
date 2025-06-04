a6961543b6ac9ad3b9a1d3b90304e6d567f0d3cb
nilopro <danilocoding@pm.me>
2025-06-04 14:14:09 -0300

    fix trigger

--- apps/web/src/components/sidebar/app-sidebar.tsx
+++ apps/web/src/components/sidebar/app-sidebar.tsx
@@ -15,6 +15,7 @@ import {
   SidebarHeader,
   SidebarMenu,
   SidebarMenuItem,
+  SidebarTrigger,
 } from "@v1/ui/sidebar";
 import { Logo } from "../logo";
 import { NavMain } from "./nav-main";
@@ -52,6 +53,7 @@ export function AppSidebar({ user, ...props }: SidebarProps) {
             </div>
           </SidebarMenuItem>
         </SidebarMenu>
+        <SidebarTrigger className="absolute right-0 top-7 -translate-y-1/2 translate-x-1/2 bg-background rounded-full border z-50" />
       </SidebarHeader>
       <SidebarContent className="flex flex-col">
         <NavMain />
--- apps/web/src/components/site-header.tsx
+++ apps/web/src/components/site-header.tsx
@@ -14,8 +14,7 @@ export async function SiteHeader({
   const credits = await getUserCreditUsage();
 
   return (
-    <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-14 shrink-0 items-center border-b transition-[width,height] ease-linear relative">
-      <SidebarTrigger className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-background rounded-full border z-50" />
+    <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-14 shrink-0 items-center border-b transition-[width,height] ease-linear ">
       <div className="flex w-full items-center gap-1 px-3 sm:px-6 ml-6 lg:gap-2 ">
         <h1 className="text-sm sm:text-base font-medium truncate">{title}</h1>
         <div className="ml-auto flex items-center gap-2">
