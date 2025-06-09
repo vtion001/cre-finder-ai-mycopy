0cbe5cc22198b53f299ba6f1f0c2648471758649
nilopro <danilocoding@pm.me>
2025-06-09 20:29:27 -0300

    fix import error

--- apps/web/src/app/[locale]/(sidebar)/dashboard/search/page.tsx
+++ apps/web/src/app/[locale]/(sidebar)/dashboard/search/page.tsx
@@ -1,12 +1,5 @@
-import { LicenseWarning } from "@/components/license-warning";
 import { PreviewSearchInterface } from "@/components/preview-search-interface";
-import { SearchLoading } from "@/components/search-loading";
-import { searchParamsCache } from "@/lib/nuqs/property-search-params";
-import {
-  getAssetTypes,
-  getUserLicenses,
-  getUserLicensesByAssetType,
-} from "@v1/supabase/cached-queries";
+import { getAllLicenses, getAssetTypes } from "@v1/supabase/cached-queries";
 import type { Metadata } from "next";
 import type { SearchParams } from "nuqs";
 
@@ -21,7 +14,7 @@ export default async function Page({
   searchParams: SearchParams;
 }) {
   const { data: assetTypes } = await getAssetTypes();
-  const { data: licenses } = await getUserLicensesByAssetType();
+  const { data: licenses } = await getAllLicenses();
 
   const unusedAssetTypes = assetTypes?.filter((type) => {
     return !licenses?.some((license) => license.asset_type_slug === type.slug);
